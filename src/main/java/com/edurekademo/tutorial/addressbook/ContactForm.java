ab181de2867308e8da69155ce2dfd478cd5e2aab
theitern <111192083+theitern@users.noreply.github.com>
2022-10-18 19:39:28 -0500

    Update ContactForm.java

--- src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
@@ -76,7 +76,7 @@ public class ContactForm extends FormLayout {
      */
     public void save(Button.ClickEvent event) {
         try {
-            // Commit the fields from UI to DAO
+            // Commit the fields from UI to DAO to see what happens
             formFieldBindings.commit();
 
             // Save DAO to backend with direct synchronous service API
